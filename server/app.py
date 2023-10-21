634c63dc87805ed03d8d36a5bf87c40a93f1224e
mark-kibo <kibochamark@gmail.com>
2023-10-21 08:39:29 -0700

    completed assignment

--- server/app.py
+++ server/app.py
@@ -3,6 +3,7 @@
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
+from flask_cors import CORS
 
 from models import db, Plant
 
@@ -13,15 +14,72 @@ app.json.compact = True
 
 migrate = Migrate(app, db)
 db.init_app(app)
+CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants = Plant.query.all()
+
+        plants_to_pass=[]
+        for plant in plants:
+            plants_dict={
+                "id":plant.id,
+                "name":plant.name,
+                "image":plant.image,
+                "price":plant.price
+            
+            }
+            plants_to_pass.append(plants_dict)
+        response= make_response(jsonify(plants_to_pass), 200)
+
+        return response
+    
+    def post(self):
+        name= request.form.get("name")
+        # print(name)
+        image= request.form.get("image")
+        price= request.form.get("price")
+
+        plant=Plant(name=name, image=image, price=price)
+        db.session.add(plant)
+        db.session.commit()
+
+        plants_dict={
+                "id":plant.id,
+                "name":plant.name,
+                "image":plant.image,
+                "price":plant.price
+            
+            }
+        return make_response(jsonify(plants_dict), 201)
+
+api.add_resource(Plants, "/plants")
 
 class PlantByID(Resource):
-    pass
-        
+    def get(self, id):
+        plant=Plant.query.get(id)
+        response =None
+        if not plant:
+            response = make_response("plant does not exist", 200)
+            return response
+        plants_dict={
+                "id":plant.id,
+                "name":plant.name,
+                "image":plant.image,
+                "price":plant.price
+            
+            }
+        response=make_response(jsonify(plants_dict), 200)
+        return response
+    
+
+    
+api.add_resource(PlantByID, "/plants/<int:id>")   
+   
+
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -7,3 +7,7 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String(20))
+
+    image = db.Column(db.String(255))  # I increased the length to 255 for image URLs, adjust as needed
+    price = db.Column(db.DECIMAL(10, 2))
